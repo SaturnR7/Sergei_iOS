eebb4188115b47f9a44bb58fb10d36610f50767f
SaturnR7 <hidemasa.kobayashi@gmail.com>
2025-06-21 12:16:43 +0900

    Initial Commit

---
+++ Sergei/SergeiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SergeiApp.swift
+//  Sergei
+//
+//  Created by Hidemasa Kobayashi on 2025/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SergeiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
